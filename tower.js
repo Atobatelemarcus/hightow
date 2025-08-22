6beed1f7697070dba298ddd4bcf9784e6752c72f
Atobatelemarcus <marcatobz3@gmail.com>
2025-08-22 08:51:31 +0100

    Add files via upload

---
+++ tower.js
@@ -0,0 +1,6 @@
+document.getElementById("sidebtn").addEventListener("click", ()=>{
+  document.getElementById("sideBar").style.height="250px";
+})
+document.getElementById("close").addEventListener("click", ()=>{
+  document.getElementById("sideBar").style.height="0px";
+})
